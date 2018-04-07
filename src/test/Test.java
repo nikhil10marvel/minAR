d87bf216fb6c1bcee575210e3ecdcd0087305bb6
Nikhil <nikhil10marvel@github.com>
2018-04-07 11:09:26 +0530

    Updated JavaDoc

--- src/test/Test.java
+++ src/test/Test.java
@@ -9,12 +9,12 @@ public class Test {
             String archive_name = args[1];
             if(args[0].equals("-a")){
                 String diriectory_to_be_archive = args[2];
-                MinAR.toggleFlags(MinAR._default_no_enc);
+                MinAR.toggleFlags(MinAR._default_enc_file);
                 MinAR.outputArchive(diriectory_to_be_archive, archive_name);
             } else if(args[0].equals("-e")){
                 String extract_directory = args[2];
                 String key = args[3];
-                MinAR.toggleFlags(MinAR._default_no_enc);
+                MinAR.toggleFlags(MinAR._default_enc_file);
                 MinAR.extractArchive(archive_name, extract_directory, key);
             } else {
                 System.out.println("Invalid arguments");
